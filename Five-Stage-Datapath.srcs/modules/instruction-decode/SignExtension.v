85f51460cc32b352e70df409b18202cd0aa53861
Vins <sharmavins23@gmail.com>
2020-07-21 17:08:56 -0400

    🐛 fixed sign extension for negative values

--- Five-Stage-Datapath.srcs/modules/instruction-decode/SignExtension.v
+++ Five-Stage-Datapath.srcs/modules/instruction-decode/SignExtension.v
@@ -4,7 +4,9 @@
 // Instruction Stage: Instruction Decode
 //
 // Description: Concatenates and extends the immediate value of the instruction
-//              from a 16 bit value to a 32 bit value.
+//              from a 16 bit value to a 32 bit value. Inserts 16 binary values
+//              after the 16th sign bit of the immediate value in order to
+//              "extend" the value. Values are 0 if positive and 1 if negative.
 ////////////////////////////////////////////////////////////////////////////////
 
 
@@ -16,7 +18,12 @@ module SignExtension(
     );
 
     always @(*) begin
-        // Concatenation to size increase
-        immediateExtended = {{16{1'b0}}, immediate};
+        // Check sign of immediate value
+        if (immediate[15] == 0) // Positive
+            // Simple concatenation to size increase
+            immediateExtended = {{16{1'b0}}, immediate};
+        else
+            // Insert 16 ones to size increase
+            immediateExtended = {{16{1'b1}}, immediate};
     end
 endmodule

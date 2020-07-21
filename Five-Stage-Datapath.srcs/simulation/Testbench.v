8ce954fb2f0a545c8f229fcaa355d07f41ec72b3
Vins <sharmavins23@gmail.com>
2020-07-21 19:52:43 -0400

    ⌚ update simulation length

--- Five-Stage-Datapath.srcs/simulation/Testbench.v
+++ Five-Stage-Datapath.srcs/simulation/Testbench.v
@@ -73,13 +73,29 @@ module Testbench();
             // Second instruction enters Instruction Fetch
 
         #10; // Positive edge of the third clock cycle /////////////////////////
+            // First instruction enters Execution
             // Second instruction enters Instruction Decode
+            // Third instruction enters Instruction Fetch
 
         #10; // Positive edge of the fourth clock cycle ////////////////////////
-            // All instructions have completed current processing.
+            // First instruction has completed.
+            // Second instruction enters Execution
+            // Third instruction enters Instruction Decode
+            // Fourth instruction enters Instruction Fetch
 
-        #15; // Positive edge of the fifth clock cycle /////////////////////////
+        #10; // Positive edge of the fifth clock cycle /////////////////////////
+            // Second instruction has completed.
+            // Third instruction enters Execution
+            // Fourth instruction enters Instruction Decode
+
+        #10; // Positive edge of the sixth clock cycle /////////////////////////
+            // Third instruction has completed.
+            // Fourth instruction enters Execution
         
+        #10; // Positive edge of the seventh clock cycle ///////////////////////
+            // All instructions have completed.
+        
+        #5;
         enable = 0; // Stop clock motion
     end
 endmodule

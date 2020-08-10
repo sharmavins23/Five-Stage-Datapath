88ea10857c1456fe2b9a8665591ba02728149912
Vins <sharmavins23@gmail.com>
2020-08-10 18:19:05 -0400

    ➕ updated sign extension module with control

--- Five-Stage-Datapath.srcs/modules/instruction-decode/SignExtension.v
+++ Five-Stage-Datapath.srcs/modules/instruction-decode/SignExtension.v
@@ -4,19 +4,26 @@
 // Instruction Stage: Instruction Decode
 //
 // Description: Concatenates and extends the immediate value of the instruction
-//              from a 16 bit value to a 32 bit value. Inserts 16 binary values
-//              after the 16th sign bit of the immediate value in order to
-//              "extend" the value. Values are 0 if positive and 1 if negative.
+//              from a 16 bit value to a 32 bit value.
+//              An input signal determines whether the extended value is signed
+//              or unsigned, and if signed, the value is concatenated properly.
+//              Otherwise, the value is concatenated with zeroes alone.
 ////////////////////////////////////////////////////////////////////////////////
 
 
 module SignExtension(
     // Inputs
     input [15:0] immediate,
+    input signedExtension,
     // Outputs
     output reg [31:0] immediateExtended
     );
 
     // Concatenate the immediate value with the first bit to get proper sign
-    always @(*) immediateExtended = {{17{immediate[15]}}, immediate[14:0]};
+    always @(*) begin
+        if (signedExtension) 
+            immediateExtended = {{17{immediate[15]}}, immediate[14:0]};
+        else
+            immediateExtended = {{16{1'b0}}, immediate};
+    end
 endmodule

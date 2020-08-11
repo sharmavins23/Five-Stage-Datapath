a7085cf08221d456b43355c8812ff3f6f2b38516
Vins <sharmavins23@gmail.com>
2020-08-11 02:53:09 -0400

    ✅ added instruction memory data

--- Five-Stage-Datapath.srcs/helper/binToHexConverter.py
+++ Five-Stage-Datapath.srcs/helper/binToHexConverter.py
@@ -17,15 +17,59 @@ class binToHexConverter:
         # Sanitize data input with spaces and newlines
         data = data.replace("\n", "")
         data = data.replace(" ", "")
-        
-        bits = len(data)*4 # Calculate amt of bits
-        binData = binToHexConverter.hexToBin(data, bits) # Get binary data
-        
+
+        bits = len(data)*4  # Calculate amt of bits
+        binData = binToHexConverter.hexToBin(data, bits)  # Get binary data
+
         itr = 0
         # Iterate through all values, printing spaces and newlines if needed
         for char in binData:
             print(char, end="")
             itr += 1
 
-            if itr % 32 == 0: print("")
-            elif itr % 8 == 0: print(" ", end="")
+            if itr % 32 == 0:
+                print("")
+            elif itr % 8 == 0:
+                print(" ", end="")
+
+
+# Instruction Memory data for CMPEN331's final project
+projectData = """
+3c010000
+34240050
+0c00001b
+20050004
+ac820000
+8c890000
+01244022
+20050003
+20a5ffff
+34a8ffff
+39085555
+2009ffff
+312affff
+01493025
+01494026
+01463824
+10a00003
+00000000
+08000008
+00000000
+2005ffff
+000543c0
+00084400
+00084403
+000843c2
+08000019
+00000000
+00004020
+8c890000
+01094020
+20a5ffff
+14a0fffc
+20840004
+03e00008
+00081000
+"""
+
+binToHexConverter.convertPrint(projectData)

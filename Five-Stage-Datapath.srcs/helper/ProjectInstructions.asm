e92ece5587816ddd38d8326e22d135477359a9d1
Vins <sharmavins23@gmail.com>
2020-08-11 18:32:01 -0400

    🎨 indenting with tabs

--- Five-Stage-Datapath.srcs/helper/ProjectInstructions.asm
+++ Five-Stage-Datapath.srcs/helper/ProjectInstructions.asm
@@ -2,57 +2,57 @@
 # With the MARS MIPS simulator, one can step through the individual instructions and see how they function.
 
 main:	
-        lui		$at, 0
-        ori		$a0, $at, 80
-        jal		sum
-    
+		lui			$at, 0
+		ori			$a0, $at, 80
+		jal			sum
+	
 dslot1:
-        addi	$a1, $zero, 4
+		addi		$a1, $zero, 4
 return:
-        sw		$v0, 0($a0)
-        lw		$t1, 0($a0)
-        sub		$t0, $t1, $a0
-        addi	$a1, $zero, 3
+		sw			$v0, 0($a0)
+		lw			$t1, 0($a0)
+		sub			$t0, $t1, $a0
+		addi		$a1, $zero, 3
 loop2:
-        addi	$a1, $a1, -1
-        ori		$t0, $a1, 0xffff	# Immediate value: 65535
-        xori	$t0, $t0, 0x5555	# Immediate value: 21845
-        addi	$t1, $zero, -1
-        andi	$t2, $t1, 0xffff	# Immediate value: 65535
-        or		$a2, $t2, $t1
-        xor		$t0, $t2, $t1
-        and		$a3, $t2, $a2
-        beq		$a1, $zero, shift
-    
+		addi    	$a1, $a1, -1
+		ori	    	$t0, $a1, 0xffff 		# Immediate value: 65535
+		xori    	$t0, $t0, 0x5555 		# Immediate value: 21845
+		addi    	$t1, $zero, -1
+		andi    	$t2, $t1, 0xffff 		# Immediate value: 65535
+		or	    	$a2, $t2, $t1
+		xor	    	$t0, $t2, $t1
+		and 		$a3, $t2, $a2
+		beq	    	$a1, $zero, shift
+	
 dslot2:
-        nop
-        j		loop2
+		nop
+		j			loop2
 
 dslot3:
-        nop
+		nop
 shift:
-        addi	$a1, $zero, -1
-        sll		$t0, $a1, 15
-        sll		$t0, $t0, 16
-        sra		$t0, $t0, 16
-        srl		$t0, $t0, 15
-    
+		addi 		$a1, $zero, -1
+		sll 		$t0, $a1, 15
+		sll 		$t0, $t0, 16
+		sra 		$t0, $t0, 16
+		srl 		$t0, $t0, 15
+	
 finish:
-        j		finish
-    
+		j 			finish
+	
 dslot4:
-        nop
+		nop
 sum:
-        add		$t0, $zero, $zero
+		add 		$t0, $zero, $zero
 loop:
-        lw		$t1, 0($a0)
+		lw 			$t1, 0($a0)
 stall:
-        add		$t0, $t0, $t1
-        addi	$a1, $a1, -1
-        bne		$a1, $0, loop
+		add 		$t0, $t0, $t1
+		addi 		$a1, $a1, -1
+		bne 		$a1, $0, loop
 dslot5:
-        addi	$a0, $a0, 4
-        jr		$ra
-    
+		addi 		$a0, $a0, 4
+		jr  		$ra
+	
 dslot6:
-        sll		$v0, $t0, 0
+		sll 		$v0, $t0, 0
